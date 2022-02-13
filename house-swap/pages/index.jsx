45fe6f083eb30be608ffb2d58bbe45c5183404e8
Kekooo <karam.salloum@new10.com>
2022-02-13 16:39:46 +0100

    feat: login and register pages

---
+++ house-swap/pages/index.jsx
@@ -0,0 +1,17 @@
+import { userService } from "services";
+import { Link } from "components";
+
+export default Home;
+
+function Home() {
+  return (
+    <div className="p-4">
+      <div className="container">
+        <h1>Hi {userService.userValue?.firstName}!</h1>
+        <p>
+          <Link href="/users">Manage Users</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
